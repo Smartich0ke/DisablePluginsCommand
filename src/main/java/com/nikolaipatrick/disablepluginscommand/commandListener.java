885fc9350fbe8d265ee567367481c438c73b25fe
Nikolai Patrick <nikolaipatricknp@gmail.com>
2022-08-19 19:56:23 +0930

    block bukkit command aliases

--- src/main/java/com/nikolaipatrick/disablepluginscommand/commandListener.java
+++ src/main/java/com/nikolaipatrick/disablepluginscommand/commandListener.java
@@ -10,15 +10,15 @@ public class commandListener implements Listener {
     public void playerRunCommand (PlayerCommandPreprocessEvent e) {
         Player p = e.getPlayer();
         String cmd = e.getMessage();
-        if (cmd.equalsIgnoreCase("/plugins") || cmd.equalsIgnoreCase("/pl")) {
+        if (cmd.equalsIgnoreCase("/plugins") || cmd.equalsIgnoreCase("/pl") || cmd.equalsIgnoreCase("/bukkit:pl") || cmd.equalsIgnoreCase("/bukkit:plugins")) {
             if (DisablePluginsCommand.getPlugin(DisablePluginsCommand.class).getConfig().getBoolean("disable-plugins-cmd")) {
                 if (!p.hasPermission("disablePluginsCmd.bypass") && !p.isOp()) {
                     e.setCancelled(true);
-                    p.sendMessage("§cSorry, but you don't have the necessary permission to run that command!");
+                    p.sendMessage("§cSorry, but you don't have the necessary permission to run that command!");//TODO: replace permission string with user-customizable one
                 }
             }
         }
-        if (cmd.equalsIgnoreCase("/version") || cmd.equalsIgnoreCase("/ver")) {
+        if (cmd.equalsIgnoreCase("/version") || cmd.equalsIgnoreCase("/ver") || cmd.equalsIgnoreCase("/bukkit:version") || cmd.equalsIgnoreCase("/bukkit:ver")) {
             if (DisablePluginsCommand.getPlugin(DisablePluginsCommand.class).getConfig().getBoolean("disable-version-cmd")) {
                 if (!p.hasPermission("disablePluginsCmd.bypass") && !p.isOp()) {
                     e.setCancelled(true);
@@ -26,7 +26,7 @@ public class commandListener implements Listener {
                 }
             }
         }
-        if (cmd.equalsIgnoreCase("/about")) {
+        if (cmd.equalsIgnoreCase("/about") || cmd.equalsIgnoreCase("/bukkit:about")) {
             if (DisablePluginsCommand.getPlugin(DisablePluginsCommand.class).getConfig().getBoolean("disable-about-cmd")) {
                 if (!p.hasPermission("disablePluginsCmd.bypass") && !p.isOp()) {
                     e.setCancelled(true);
@@ -34,5 +34,6 @@ public class commandListener implements Listener {
                 }
             }
         }
+        //TODO: disable /bungee and /waterfall comands
     }
 }
